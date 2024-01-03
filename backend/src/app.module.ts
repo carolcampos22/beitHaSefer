f865078c01a94e5240557b4fd8b39596eef3c5b3
carolcampos22 <mendes.carolina2209@gmail.com>
2024-01-03 17:48:35 -0300

    Criados os módulos de usuários e produtos

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController],
+  imports: [UsersModule, ProductsModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
