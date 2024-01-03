fd2e3f2371d8e2e937b390a8babe1c405e498514
carolcampos22 <mendes.carolina2209@gmail.com>
2024-01-03 18:28:59 -0300

    Correções de erros

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,13 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { UsersService } from './users/users.service';
+import { ProductsController } from './products/products.controller';
+import { ProductsService } from './products/products.service';
 
 @Module({
   imports: [UsersModule, ProductsModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService],
+  controllers: [AppController, UsersController, ProductsController],
+  providers: [AppService, UsersService, ProductsService],
 })
 export class AppModule {}
